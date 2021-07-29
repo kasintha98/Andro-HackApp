70038a87b887acf0eafa94bbd282c3fa9ec15572
Kasintha Kalhara <kasinthakalhara@gmail.com>
2021-07-30 03:52:48 +0530

    deploy

--- backend/routes/spaceRouter.js
+++ backend/routes/spaceRouter.js
@@ -1,42 +1,35 @@
 const Space = require("../models/spaceModel");
 const router = require("express").Router();
 
+//test
 
-router.post('/addspace',async (req,res,body)=>{
+router.post("/addspace", async (req, res, body) => {
   try {
-    const space ={
-        spacetype:req.body.type,
-        capacity:req.body.capacity,
-        price:req.body.price,
-        facilities:req.body.facilities,
-    }
+    const space = {
+      spacetype: req.body.type,
+      capacity: req.body.capacity,
+      price: req.body.price,
+      facilities: req.body.facilities,
+    };
 
-    const newSpace= new Space({
-        spacetype:space.spacetype,
-        capacity:space.capacity,
-        price:space.price,
-        facilities:space.facilities
-
+    const newSpace = new Space({
+      spacetype: space.spacetype,
+      capacity: space.capacity,
+      price: space.price,
+      facilities: space.facilities,
     });
 
     const savedSpace = await newSpace.save();
     res.status(200).json(savedSpace);
-      
   } catch (error) {
     res.status(500).json({ error: error.message });
-  }  
-
-
+  }
 });
 
+router.route("/getallspaces").get((req, res) => {
+  Space.find()
+    .then((cart) => res.json(cart))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-router.route('/getallspaces').get((req,res) => {
-    Space.find()
-    .then(cart => res.json(cart))
-    .catch(err => res.status(400).json('Error: '+ err));
-  });
-
-
-
-
-module.exports=router;
+module.exports = router;
